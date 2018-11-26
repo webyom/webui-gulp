f36b068c62d75e12010d2ebe4dc25937306d916f
webyom <webyom@gmail.com>
2018-11-26 17:34:56 +0800

    0.10.0

--- tasks/conf.js
+++ tasks/conf.js
@@ -42,11 +42,13 @@ let conf;
 conf.CACHE_DIR_NAME = '.build-cache';
 conf.USE_CACHE = process.env.BUILD_CACHE != '0';
 conf.ESLINT_FIX = process.env.ESLINT_FIX == '1';
-conf.BASE_PROJECT_NAME = config.baseProjectName || 'webui-base';
 conf.PROJECT_NAME = process
   .cwd()
   .split(path.sep)
   .pop();
+conf.BASE_PROJECT_NAME
+  = config.baseProjectName
+  || (conf.PROJECT_NAME.indexOf('webui-m-') === 0 ? 'webui-m-base' : 'webui-base');
 conf.IS_BASE_PROJECT = conf.PROJECT_NAME == conf.BASE_PROJECT_NAME;
 conf.ENV = env;
 conf.VERSION_DIGEST_LEN = 4;
