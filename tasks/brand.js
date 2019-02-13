8326e9c1980eb7c9b3dd7ff278d98130055b1921
webyom <webyom@gmail.com>
2019-02-13 12:18:54 +0800

    0.64.0

---
+++ tasks/brand.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp'),
+  conf = require('./conf');
+
+gulp.task('brand-custom', function () {
+  if (!conf.BRAND_NAME) {
+    throw new Error('brand-custom: BRAND_NAME must be provided!');
+  }
+  return gulp
+    .src(['src/brand/' + conf.BRAND_NAME + '/**/_custom/**/*'], {
+      base: 'src/brand/' + conf.BRAND_NAME
+    })
+    .pipe(gulp.dest('src/' + conf.PROJECT_NAME));
+});
